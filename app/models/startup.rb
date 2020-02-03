834e6b005d21712cb65b7b20d9082ad80edd4484
github username <github email address>
2020-02-03 13:46:32 -0500

    done

--- app/models/startup.rb
+++ app/models/startup.rb
@@ -1,3 +1,58 @@
 class Startup
+    attr_accessor :name, :domain
+    attr_reader :founder
+
+    @@all = []
+
+    def initialize(name, founder, domain)
+        @name = name
+        @founder = founder
+        @domain = domain
+
+        @@all << self
+    end
+
+    def pivot(name, domain)
+        @name = name
+        @domain = domain
+    end
+
+    ### do these need to be private re: instructions for this class?
+    def self.all
+        @@all 
+    end
+
+    def self.find_by_founder(name)
+        @@all.find{|startup| startup.founder == name}
+    end
+
+    def self.domains
+        @@all.map{|startup| startup.domain}.uniq
+    end
+
+    ###ASSOCIATION METHODS
+    def funding_rounds
+        FundingRound.all.select{|rd| rd.startup == self}
+    end
+
+    def sign_contract(vc, type, amount)
+        FundingRound.new(self, vc, type, amount)
+    end
+
+    def num_funding_rounds
+        self.funding_rounds.count
+    end
+
+    def total_funds
+        self.funding_rounds.reduce(0){|total, rd| total + rd.investment}
+    end
+
+    def investors
+        self.funding_rounds.map{|rd| rd.venture_capitalist}.uniq
+    end
+
+    def big_investors
+        self.investors.select{|vc| vc.total_worth >= 1000000000}
+    end
 
 end
--- app/models/venture_capitalist.rb
+++ app/models/venture_capitalist.rb
@@ -1,3 +1,56 @@
 class VentureCapitalist
+    attr_reader :name, :total_worth
 
+    @@all = []
+
+    def initialize(name, total_worth)
+        @name = name
+        @total_worth = total_worth
+
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def self.tres_commas_club
+        @@all.select{|vc| vc.total_worth >= 1000000000}
+    end
+
+    ###ASSOCIATION METHODS
+    def offer_contract(startup, type, amount)
+        FundingRound.new(startup, self, type, amount)
+    end
+
+    def funding_rounds
+        FundingRound.all.select{|rd| rd.venture_capitalist == self}
+    end
+
+    def portfolio
+        self.funding_rounds.map{|rd| rd.startup}.uniq
+    end
+
+    def biggest_investment
+        large_investment = 0
+        largest_funding_rd = nil
+        self.funding_rounds.each{|rd|
+            if rd.investment > large_investment
+                large_investment = rd.investment
+                largest_funding_rd = rd
+            end
+        }
+        largest_funding_rd
+    end
+
+    def invested(domain)
+        Startup.all.reduce(0){|total_invested, startup|
+            # total_invested + startup.total_funds
+            if startup.domain == domain
+                total_invested + startup.total_funds
+            else 
+                total_invested
+            end
+        }
+    end
 end

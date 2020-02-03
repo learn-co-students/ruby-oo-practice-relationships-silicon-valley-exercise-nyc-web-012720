2010339f5dcb06a7ad392b1da2ab6093fe295734
adit313 <app2116@gmail.com>
2020-02-03 14:16:21 -0500

    Done.

--- app/models/funding_round.rb
+++ app/models/funding_round.rb
@@ -1,3 +1,21 @@
 class FundingRound
 
+attr_accessor :type, :investment
+attr_reader :startup, :venture_capitalist
+
+@@all = []
+
+	def initialize(type, startup, venture_capitalist, investment)
+		@@all << self
+		@type = type
+		@startup = startup
+		@venture_capitalist = venture_capitalist
+		@investment = investment
+	end
+
+	def self.all
+		return @@all
+	end
+
+
 end

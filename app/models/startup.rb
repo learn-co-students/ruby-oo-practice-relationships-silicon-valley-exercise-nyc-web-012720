fb0b8fdb72478918c69da8fcaa98293a78f6ce80
osgoodgunawan <ogunawan@ucsd.edu>
2020-02-03 17:43:44 -0500

    Done.

--- app/models/startup.rb
+++ app/models/startup.rb
@@ -1,3 +1,77 @@
 class Startup
 
+    
+    attr_reader :founder, :name, :domain
+    # attr_accessor :funding_rounds
+
+    @@all=[]
+
+    def initialize (name,founder,domain)
+        @name =name
+        @founder=founder
+        @domain=domain
+        @@all << self
+        # @funding_rounds=0
+        # @total_funds=0
+    end
+
+    def pivot(name,domain)
+        @domain=domain
+        @name=name
+    end
+
+    def self.all
+        @@all
+    end
+
+
+    def self.find_by_founder(name)
+        @@all.find {|n| n.founder==name}
+    end
+
+    def self.domains
+        @@all.map {|d| d.domain}
+    end
+
+    def sign_contract(vc,type,amount)
+        FundingRound.new(self, vc, type, amount)
+        # @funding_rounds+=1
+        # @total_funds+=amount
+    end
+
+
+    def num_funding_rounds
+        # @funding_rounds
+        FundingRound.all.select{|fr| fr.startup == self}.count
+    end
+
+    def total_funds
+        # @total_funds
+        startup_fr=FundingRound.all.select{|fr| fr.startup == self}
+        startup_fr.reduce(0) {|sum ,i| sum + i.investment}
+    end
+
+    def investors
+
+         startsups_frs = FundingRound.all.select{|fr| fr.startup == self}
+
+       startsups_frs.map{|fr| fr.venture_capitalist }.uniq
+
+    end
+
+    def big_investors
+
+        self.investors.select {|investor| investor.total_worth>1000000000}
+
+    end
+
+
+
+
+
+
+
+
+
+
 end
--- tools/console.rb
+++ tools/console.rb
@@ -6,6 +6,26 @@ end
 # Insert code here to run before hitting the binding.pry
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
+google=Startup.new("Google","Serg","SF")
+amazon=Startup.new("Amazon","Bezoz","SE")
+apple=Startup.new("Apple","Jobs","SF")
+
+
+flatiron=VentureCapitalist.new("Flatiron",10000000)
+appAcademy=VentureCapitalist.new("AA",108000000)
+fullStack=VentureCapitalist.new("Fullstack",1000000000000)
+
+
+
+
+sequoia=FundingRound.new(google,flatiron,"Series B",-111111.123)
+sequoia1=FundingRound.new(google,flatiron,"Series B",11000000.123)
+
+
+forbes=FundingRound.new(amazon,appAcademy,"Series C",50000)
+
+gvm=FundingRound.new(apple,fullStack,"Series D",50000.101)
+
 
 binding.pry
 0 #leave this here to ensure binding.pry isn't the last line

dc22b6782fc1d623df8619a189cbf6af07bc23f4
github username <github email address>
2020-02-04 15:38:19 -0500

    Done.

--- app/models/venture_capitalist.rb
+++ app/models/venture_capitalist.rb
@@ -1,3 +1,66 @@
 class VentureCapitalist
+    attr_accessor :name, :total_worth
+
+    @@all = []
+
+    def initialize(name, total_worth)
+        @name = name
+        @total_worth = total_worth 
+
+        @@all << self 
+    end 
+
+    def self.all 
+        @@all 
+    end 
+
+    def self.tres_commas_club
+        self.all.select do |vc| 
+            vc.total_worth >= 1000000000
+        end 
+    end 
+
+    def offer_contract(start_up, type, investment)
+        FundingRound.new(start_up, self, type, investment)
+    end 
+
+    def funding_rounds 
+        FundingRound.all.select do |round|
+            round.venture_capitalist == self
+        end
+    end 
+
+    def portfolio
+        funding_rounds.map do |round|
+            round.start_up
+        end.uniq
+    end 
+
+    def biggest_investment
+        list_of_investments = funding_rounds.map do |round|
+            round.investment
+        end
+
+        biggest_value = list_of_investments.max
+
+        funding_rounds.find do |round|
+            round.investment == biggest_value
+        end 
+    end
+
+    def invested(domain_string)
+        domain_total = 0 
+        funding_rounds.each do |round|
+            if round.start_up.domain == domain_string
+                domain_total += round.investment 
+            end 
+        end
+        domain_total 
+    end 
 
 end
+
+
+
+
+
--- tools/console.rb
+++ tools/console.rb
@@ -7,5 +7,30 @@ end
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
 
+startup1 = Startup.new("apple", "steve", "computers")
+startup2 = Startup.new("facebook", "mark", "social media")
+startup3 = Startup.new("microsoft", "bill", "computers")
+startup4 = Startup.new("amazon", "jeff", "shopping")
+startup5 = Startup.new("profoco", "john", "computers")
+
+vc1 = VentureCapitalist.new("James", 100)
+vc2 = VentureCapitalist.new("Joe", 70000000)
+vc3 = VentureCapitalist.new("Sally", 1000000000)
+vc4 = VentureCapitalist.new("Jess", 1000000001)
+vc5 = VentureCapitalist.new("Frank", 9999999999999999)
+
+fund1 = FundingRound.new(startup5, vc1, "Angel", 30000.00)
+fund2 = FundingRound.new(startup4, vc1, "Pre-Seed", 40000.00)
+fund3 = FundingRound.new(startup3, vc1, "Series-A", 50000.00)
+fund4 = FundingRound.new(startup2, vc1, "Series-B", 60000.00)
+fund5 = FundingRound.new(startup1, vc5, "Series-C", 70000.00)
+
+
+
+
+
+
+
+
 binding.pry
 0 #leave this here to ensure binding.pry isn't the last line

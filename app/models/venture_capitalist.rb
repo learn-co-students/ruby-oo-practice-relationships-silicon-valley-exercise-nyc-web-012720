7925d08fd809348b464e1c3d02196f3b3076114f
davidmlee4 <davidmlee4@gmail.com>
2020-02-04 13:52:21 -0500

    Done.

--- app/models/venture_capitalist.rb
+++ app/models/venture_capitalist.rb
@@ -1,3 +1,48 @@
 class VentureCapitalist
 
+    attr_accessor :name, :startup, :total_worth
+    @@all = []
+
+    def initialize(name,total_worth)
+        @name = name
+        @total_worth = total_worth
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def self.tres_commas_club
+        @@all.select {|vc| vc.total_worth > 1000000000}
+    end
+
+    def offer_contract(startup,type,investment)
+        FundingRound.new(startup,self,type,investment)
+    end
+
+    def funding_rounds#same thing as meals
+        FundingRound.all.select {|fr|fr.venture_capitalist == self}
+    end
+
+    def portfolio 
+        self.funding_rounds.map {|fr|fr.startup}.uniq
+    end
+
+    def biggest_investment
+        investments = self.funding_rounds.map {|fr|fr.investment}
+        largest_investment = investments.max
+        FundingRound.all.find {|fr|fr.investment == largest_investment}
+    end
+
+    def invested(domain)
+        funding_round_by_domain = self.funding_rounds.find {|fr|fr.startup.domain == domain}
+        startup = funding_round_by_domain.startup
+        startup_funding_rounds = startup.num_funding_rounds
+        total_invested = startup_funding_rounds.map {|fr|fr.investment}.sum
+        total_invested
+    end
+
+    
+
 end
--- tools/console.rb
+++ tools/console.rb
@@ -7,5 +7,28 @@ end
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
 
+tesla = Startup.new("Tesla","Elon Musk","tesla.com")
+facebook = Startup.new("Facebook","Mark Zuckerberg","facebook.com")
+snapchat = Startup.new("Snapchat","Evan Spiegel","snapchat.com")
+
+snapchat.pivot("www.snapchat.com","Snapchat, Inc.")
+Startup.all 
+Startup.domains
+
+sequoia = VentureCapitalist.new("Sequoia Capital",1400000000000)
+accel = VentureCapitalist.new("Accel",4600000000)
+some_vc = VentureCapitalist.new("Some VC",12000000)
+
+tcc = VentureCapitalist.tres_commas_club
+
+tesla_angel = FundingRound.new(tesla,sequoia,"Angel",99000000)
+snapchat_seed = FundingRound.new(snapchat,accel,"Seed",32000000)
+facebook_a = FundingRound.new(facebook,some_vc,"Series A",12000000)
+facebook_b = FundingRound.new(facebook,sequoia,"Series B",1000000000)
+
+sequoia.offer_contract(snapchat,"Series A",1000000)
+tesla.sign_contract(sequoia,"Pre-Seed",20000000)
+
+
 binding.pry
 0 #leave this here to ensure binding.pry isn't the last line
